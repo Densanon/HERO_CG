981fdb3d85c2bafe782be49a38e4b1a492ba2ace
Densanon <jordan.ezell@yahoo.com>
2020-12-15 12:22:24 -0800

    Photon Chat Setup
    I setup Photon Char system.
    Some reason it isn't actually sending a DM.

---
+++ HERO_CG/Assets/Scripts/Photon/PhotonChatController.cs
@@ -0,0 +1,115 @@
+﻿using ExitGames.Client.Photon;
+using Photon.Chat;
+using Photon.Pun;
+using System;
+using UnityEngine;
+
+public class PhotonChatController : MonoBehaviour, IChatClientListener
+{
+    [SerializeField] private string nickName;
+    private ChatClient chatClient;
+
+    #region Unity Methods
+    private void Awake()
+    {
+        nickName = PlayerPrefs.GetString("USERNAME");   
+    }
+
+    private void Start()
+    {
+        chatClient = new ChatClient(this);
+        ConnectToPhotonChat();
+    }
+
+    void Update()
+    {
+        chatClient.Service();
+    }
+    #endregion
+
+    #region Private Methods
+    private void ConnectToPhotonChat()
+    {
+        Debug.Log("Connecting to Photon Chat");
+        chatClient.AuthValues = new Photon.Chat.AuthenticationValues(nickName);
+        ChatAppSettings chatSettings = PhotonNetwork.PhotonServerSettings.AppSettings.GetChatSettings();
+        chatClient.ConnectUsingSettings(chatSettings);
+    }
+    #endregion
+
+    #region Public Methods
+    public void SendDirectMessage(string recipient, string message)
+    {
+        chatClient.SendPrivateMessage(recipient, message);
+    }
+    #endregion
+
+    #region Photon Chat Callback
+
+    public void DebugReturn(DebugLevel level, string message)
+    {
+        
+    }
+
+    public void OnDisconnected()
+    {
+        Debug.Log($"You have Disconnected to Photon Chat.");
+    }
+
+    public void OnConnected()
+    {
+        Debug.Log($"You have connected to Photon Chat.");
+        SendDirectMessage("Ezell7", "Hi Ezell");
+    }
+
+    public void OnChatStateChange(ChatState state)
+    {
+       
+    }
+
+    public void OnGetMessages(string channelName, string[] senders, object[] messages)
+    {
+        
+    }
+
+    public void OnPrivateMessage(string sender, object message, string channelName)
+    {
+        if (string.IsNullOrEmpty(message.ToString()))
+        {
+            // Channel Name format [Sender : Recipient]
+            string[] splitNames = channelName.Split(new char[] { ',' });
+            string senderName = splitNames[0];
+            if (sender.Equals(senderName, StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.Log($"{sender}: {message}");
+            }
+            Debug.Log($"{sender}: {message}");
+        }
+    }
+
+    public void OnSubscribed(string[] channels, bool[] results)
+    {
+        
+    }
+
+    public void OnUnsubscribed(string[] channels)
+    {
+        
+    }
+
+    public void OnStatusUpdate(string user, int status, bool gotMessage, object message)
+    {
+        
+    }
+
+    public void OnUserSubscribed(string channel, string user)
+    {
+        
+    }
+
+    public void OnUserUnsubscribed(string channel, string user)
+    {
+        
+    }
+    #endregion
+}
