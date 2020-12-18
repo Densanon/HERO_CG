affc3754ce3ce88e8cfb1e7979e83d2afa8b0999
Densanon <jordan.ezell@yahoo.com>
2020-12-18 12:56:39 -0800

    Friend System Update
    Altered the UIFriend, and UIDisplayFriends. This was to make sure that the prefab of the friends are actually getting instantiated for the full list of friends.

--- HERO_CG/Assets/Scripts/UI/UIDisplayFriends.cs
+++ HERO_CG/Assets/Scripts/UI/UIDisplayFriends.cs
@@ -8,14 +8,23 @@ public class UIDisplayFriends : MonoBehaviour
 {
     [SerializeField] private Transform friendContainer;
     [SerializeField] private UIFriend uiFriendPrefab;
+    [SerializeField] private RectTransform contentRect;
+    [SerializeField] private Vector2 orginalSize;
+    [SerializeField] private Vector2 increaseSize;
+
     private void Awake()
     {
+        contentRect = friendContainer.GetComponent<RectTransform>();
+        orginalSize = contentRect.sizeDelta;
+        increaseSize = new Vector2(0, uiFriendPrefab.GetComponent<RectTransform>().sizeDelta.y);
         PhotonFriendController.OnDisplayFriends += HandleDisplayFriends;
+        PhotonChatFriendController.OnDisplayFriends += HandleDisplayChatFriends;
     }
 
     private void OnDestroy()
     {
         PhotonFriendController.OnDisplayFriends -= HandleDisplayFriends;
+        PhotonChatFriendController.OnDisplayFriends -= HandleDisplayChatFriends;
     }
 
     private void HandleDisplayFriends(List<FriendInfo> friends)
@@ -29,6 +38,25 @@ public class UIDisplayFriends : MonoBehaviour
         {
             UIFriend uifriend = Instantiate(uiFriendPrefab, friendContainer);
             uifriend.Initialize(friend);
+        }
+    }
+
+    private void HandleDisplayChatFriends(List<string> friends)
+    {
+        Debug.Log("UI remove prior friends displayed");
+        foreach (Transform child in friendContainer)
+        {
+            Destroy(child.gameObject);
+        }
+
+        Debug.Log($"UI instantiate friends display {friends.Count}");
+        contentRect.sizeDelta = orginalSize;
+
+        foreach (string friend in friends)
+        {
+            UIFriend uifriend = Instantiate(uiFriendPrefab, friendContainer);
+            uifriend.Initialize(friend);
+            contentRect.sizeDelta += increaseSize;
         }
     }
 }
